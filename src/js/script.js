5a468f166ae4a4f41e020bd97119cf96134f62da
dombatersesat <benedkudat@gmail.com>
2024-01-15 14:46:32 +0700

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+//
+
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const navbarfixed = header.offsetTop;
+
+
+    if(window.pageYOffset > navbarfixed){
+        header.classList.add('navbar-fixed');
+    }else{header.classList.remove('navbar-fixed');
+    }
+}
+
+
+//button hamburger//
+const hamburger =document.querySelector('#hamburger');
+const navMenu =document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click',function(){
+    hamburger.classList.toggle('active');
+  
+navMenu.classList.toggle('hidden');
+
+})
